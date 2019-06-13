041de3bd900b54ef9c138e692999103cae380cc3
leonardoroman <lr534@scarletmail.rutgers.edu>
2019-06-12 22:21:49 -0400

    app updated

--- HostessApp/src/model/HostApp.java
+++ HostessApp/src/model/HostApp.java
@@ -13,41 +13,34 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class HostApp 
-extends Application 
+public class HostApp extends Application 
 {
-	private static final Logger logger 
-	= Logger.getLogger(HostApp.class.getName());
-
+	private static final Logger logger = Logger
+			.getLogger(HostApp.class.getName());
+	
 	@Override
-	public void start(Stage primaryStage) 
-			throws IOException 
+	public void start(Stage primaryStage) throws IOException 
 	{
-	    
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass()
 				.getResource("/view/diningroom.fxml"));
 		AnchorPane root = (AnchorPane)loader.load();
 
-
-		HostController userController = 
-				loader.getController();
+		HostController userController = loader.getController();
 		userController.start(primaryStage);
-		
+
 		Scene scene = new Scene(root,600,600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
+
 	/**
 	 * 
 	 * @param args
 	 * @throws UnknownHostException 
 	 */
-	public static void main(String[] args) 
-			throws UnknownHostException 
+	public static void main(String[] args) throws UnknownHostException 
 	{
-		// Get Host IP address for reference.		
 		launch(args);
 	}
 }
