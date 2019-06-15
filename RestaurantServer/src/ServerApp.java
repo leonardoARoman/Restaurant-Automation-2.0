b855926a18f51a19d19c4ec987ea661c2f288654
Roman <lr534@scarletmail.rutgers.edu>
2019-06-15 16:48:14 -0400

    new stub library added

--- RestaurantServer/src/ServerApp.java
+++ RestaurantServer/src/ServerApp.java
@@ -1,23 +1,41 @@
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import com.service.ServerInterceptorHandler;
 import com.service.ServiceStub;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.restaurantnetworkapp.Table;
 
 public class ServerApp {
 	private static final Logger logger 
 	= Logger.getLogger(ServerApp.class.getName());
+	private static final int numberOfTables = 12;
 
+	private static Collection<Table> RestaurantTables()
+	{
+		Collection<Table> tables = new ArrayList<Table>();
+		for(int i = 0; i < numberOfTables; i++)
+		{
+			tables.add(Table
+					.newBuilder()
+					.setTableID(i+1)
+					.setStatus(Table.TableState.CLEAN)
+					.build());
+		}
+		return tables;
+	}
+	
 	public static void main(String[] args) 
 			throws IOException, InterruptedException 
 	{
 		logger.info(InetAddress.getLocalHost().toString());
 		Server server = ServerBuilder
 				.forPort(8080)
-				.addService(ServiceStub.getInstance())
+				.addService(ServiceStub.getInstance(RestaurantTables()))
 				.intercept(new ServerInterceptorHandler())
 				.handshakeTimeout(10, TimeUnit.SECONDS)
 				.build();
