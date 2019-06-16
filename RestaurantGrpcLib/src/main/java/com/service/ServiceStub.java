fa9bc290d892e70958b86906f39cc2d05ed5c8fe
Roman <lr534@scarletmail.rutgers.edu>
2019-06-16 14:33:16 -0400

    New grpc added and Kitchen API created

--- RestaurantGrpcLib/src/main/java/com/service/ServiceStub.java
+++ RestaurantGrpcLib/src/main/java/com/service/ServiceStub.java
@@ -9,6 +9,8 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.logging.Logger;
+
+import io.grpc.restaurantnetworkapp.Order;
 import io.grpc.restaurantnetworkapp.Response;
 import io.grpc.restaurantnetworkapp.RestaurantServiceGrpc;
 import com.service.ServiceStub;
@@ -25,20 +27,23 @@ RestaurantServiceGrpc.RestaurantServiceImplBase
 	private static final Logger logger = Logger.getLogger(ServiceStub.class.getName());
 	private static ServiceStub instance;
 	private static Collection<Table> tableRecord;
+	private static Collection<Order> orderRecord;
 	private static ArrayList<Table> tables;
 	private static String DB = "./src/main/database/RestaurantDB.db";
 	private static String URL1 = "./src/main/database/tableRecord.dat";
 	private static String URL = "./database/tableRecord.dat";
+	private static String URL2 = "./database/orderRecord.dat";
 
 	private ServiceStub() 
 	{
 		tableRecord = new ArrayList<Table>();
-		//tables = new ArrayList<Table>();
+		orderRecord = new ArrayList<Order>();
 	}
 
 	private ServiceStub(Collection<Table> tableRecord)
 	{
 		this.tableRecord = tableRecord;
+		orderRecord = new ArrayList<Order>();
 	}
 
 	public static ServiceStub getInstance() throws IOException 
@@ -130,22 +135,52 @@ RestaurantServiceGrpc.RestaurantServiceImplBase
 
 		responseObserver.onCompleted();
 	}
-	
-	private Collection<Table> deserialize() throws IOException, ClassNotFoundException
+
+
+	@Override
+	public void order(Order order, 
+			StreamObserver<Response> responseObserver) 
 	{
-		FileInputStream fis = null;
-		ObjectInputStream ois = null;
-		try
+		orderRecord.add(order);
+		try 
 		{
-			fis = new FileInputStream(URL);
-			ois = new ObjectInputStream(fis);
-		} 
-		catch (FileNotFoundException e) 
+			saveOrder();
+		} catch (IOException e) 
 		{
 			e.printStackTrace();
 		}
-		Collection<Table> alp = (ArrayList<Table>)ois.readObject();
-		return alp;
+
+		Response response = Response
+				.newBuilder()
+				.setMessage("Order number "+order.getOrderNo()+" added to order record")
+				.build();
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void status(Response request, 
+			StreamObserver<Order> responseObserver) 
+	{
+		int orderNumber = Integer.parseInt(request.getMessage());
+
+		for(Order order: orderRecord) 
+		{
+			if(order.getOrderNo()==orderNumber)
+			{
+				if(order.getIsReady()) {
+					Response
+					.newBuilder()
+					.setMessage("Order is ready.");
+				}else {
+					Response
+					.newBuilder()
+					.setMessage("Order is not ready.");
+				}
+			}
+		}
+		responseObserver.onCompleted();
 	}
 
 	private static void updateTableRecord() throws IOException 
@@ -170,4 +205,46 @@ RestaurantServiceGrpc.RestaurantServiceImplBase
 			}
 		}
 	}
+
+	private static void saveOrder() throws IOException 
+	{
+		ObjectOutputStream outputStream = null;
+		FileOutputStream oututfile = null;
+		try 
+		{
+			oututfile = new FileOutputStream(URL2);
+			outputStream = new ObjectOutputStream(oututfile);
+			outputStream.writeObject(orderRecord);
+		} 
+		catch (IOException  e) 
+		{
+			e.printStackTrace();
+		}
+		finally 
+		{
+			if (outputStream != null)
+			{
+				outputStream.close();
+			}
+		}
+	}
+	/*
+	private Collection<Table> deserialize() throws IOException, ClassNotFoundException
+	{
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		try
+		{
+			fis = new FileInputStream(URL);
+			ois = new ObjectInputStream(fis);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		Collection<Table> alp = (ArrayList<Table>)ois.readObject();
+		return alp;
+	}
+	 */
+
 }
