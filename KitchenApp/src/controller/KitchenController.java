69f9fd6576a3319d9dd159cc0dffdd8f1dd2e343
leonardoroman <lr534@scarletmail.rutgers.edu>
2019-06-22 18:08:16 -0400

    kitchen app functionality updated

--- KitchenApp/src/controller/KitchenController.java
+++ KitchenApp/src/controller/KitchenController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import com.client.KitchenClient;
+
+import io.grpc.restaurantnetworkapp.Order;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
@@ -14,6 +20,8 @@ public class KitchenController {
 
 	private static Stage stage;
 	private ObservableList<String> hotLineObsList1,hotLineObsleList2,hotLineObsList3,hotLineObsList4,saladLineObsList1,saladLineObsList2;
+	private static KitchenClient clientSub;
+	private static final Logger logger = Logger.getLogger(KitchenController.class.getName());
 
 	@FXML
 	private ListView<String> orderList1,orderList2,orderList3,orderList4,saladOrderList1,saladOrderList2;
@@ -23,15 +31,22 @@ public class KitchenController {
 	 * @param mainStage
 	 */
 	public void start(Stage mainStage) {
+		clientSub = new KitchenClient("192.168.1.11",8080);
 		stage = mainStage;
 	}
 
 	public void register1() {
-
+		
 	}
 
 	public void register2() {
-
+		ArrayList<Order> orders = clientSub.getOrdersInQueue();
+		ArrayList<String> list = new ArrayList<String>();
+		for(Order o: orders) {
+			list.add(o.getMessage());
+		}
+		hotLineObsList1 = FXCollections.observableArrayList(list);
+		orderList1.setItems(hotLineObsList1);
 	}
 
 	public void register3() {
